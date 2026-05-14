99c8743fbbf81de6bff31158d1631e35d812286b
GachauDominic <gachaudominic@gmail.com>
2026-05-14 15:48:39 +0300

    Hide Port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import app from "./index"
 
 
 //an app must listen to a port and some ports are usually available while others are not (8080, 8081, 3000) are usually available
+const PORT = 8080;
 app.listen(8080, () => {
-  console.log('Server is running on the port: http://localhost:8080')
+  console.log(`Server is running on the port: http://localhost:${PORT}`);
 })
