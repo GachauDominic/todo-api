3127b9ca05e4e3a0133983d0743b9604cbae049a
GachauDominic <gachaudominic@gmail.com>
2026-06-03 19:04:21 +0300

    origin: making sure all front-ends can call the API

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const initializeApp = ()=> {
   //middleware
   // be ware that the cors shold be used after the first two express 
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE" ],
   }))
 
